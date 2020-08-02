9bdaf2e59be2f756e9947c9dff8deec895bb05a0
xiaoe <vitaszhuo@xiaoe-tech.com>
2020-08-02 23:30:42 +0800

    创建进程

--- 03/pcntl/pcntl.php
+++ 03/pcntl/pcntl.php
@@ -12,19 +12,19 @@
 //    echo $pid.PHP_EOL;
 
 //    $pid = pcntl_fork(); //创建成功会返回子进程id
-//    for($i=0;$i<5;$i++){
+    for($i=0;$i<5;$i++){
         $pid = pcntl_fork(); //创建成功会返回子进程id
         if($pid < 0){
             exit("创建失败了");
         }else if($pid > 0){
             //父进程空间，返回子进程id
-            echo "子进程id:".$a.PHP_EOL;
+            echo "子进程id:".$pid.PHP_EOL;
+
         }else {
             //返回为0子进程空间
             //子进程创建成功
-            $a =2 ;
             echo $pid . PHP_EOL;
-            echo "子进程：".$a.PHP_EOL;
+            sleep(20);
         }
-//    }
+    }
 
